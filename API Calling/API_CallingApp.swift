5d82e773e5da9ec21567c0023cd5f4d3be811f00
Colin Joyce <Cjoyce1@bsd220.org>
2023-03-02 17:04:59 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Colin Joyce on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
